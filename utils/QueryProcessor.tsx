bcd3c7ecc6d7944e65c91d2f6d7998cf03115781
Suyeon Cha <suyeonc@andrew.cmu.edu>
2024-03-14 16:18:07 -0400

    plus

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -30,7 +30,7 @@ export default function QueryProcessor(query: string): string {
     var i = arr.indexOf("plus");
     var str = arr[i + 1]
     return (
-      parseInt(arr[i - 1]) + "+" + parseInt(str)
+      (parseInt(arr[i - 1]) + parseInt(str)) + ""
     );
   }
 
